50ba56aa52e2b0cb0b8205e066dcbc7f190718fa
clinic18 <clinic18@WL-218-163.WPA.Claremont.Edu>
2018-10-17 16:27:45 -0700

    Add check 126 test

--- test/clang-tidy/abseil-make-unique.cpp
+++ test/clang-tidy/abseil-make-unique.cpp
@@ -1,14 +1,30 @@
 // RUN: %check_clang_tidy %s abseil-make-unique %t
+namespace std {
+template <typename T>
+struct default_delete {};
 
-// FIXME: Add something that triggers the check here.
-void f();
-// CHECK-MESSAGES: :[[@LINE-1]]:6: warning: function 'f' is insufficiently awesome [abseil-make-unique]
+template <typename T, typename D = default_delete<T>>
+class unique_ptr {
+ public:
+  unique_ptr();
+  ~unique_ptr();
+  explicit unique_ptr(T*);
+  template <typename U, typename E>
+  unique_ptr(unique_ptr<U, E>&&);
+  T* release();
+  void reset(T*);
+};
+}  // namespace std
 
-// FIXME: Verify the applied fix.
-//   * Make the CHECK patterns specific enough and try to make verified lines
-//     unique to avoid incorrect matches.
-//   * Use {{}} for regular expressions.
-// CHECK-FIXES: {{^}}void awesome_f();{{$}}
 
-// FIXME: Add something that doesn't trigger the check here.
-void awesome_f2();
+void Positives() {
+  std::unique_ptr<int> upi(new int(1));
+  // CHECK-MESSAGES: :[[@LINE-1]]:24: warning: Replace new with absl::make_unique [abseil-make-unique] 
+  std::unique_ptr<int> toReset;
+  toReset.reset(new int(2));
+  // CHECK-MESSAGES: :[[@LINE-1]]:3: warning: Replace reset with absl::make_unique [abseil-make-unique] 
+}
+
+void Negatives(){
+  std::unique_ptr<int> upi;
+}
