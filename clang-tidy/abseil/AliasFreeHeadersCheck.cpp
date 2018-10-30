32a19f66f54e1c413b77b58fcbc15750c8d19517
Naysh <sakmal@hmc.edu>
2018-10-29 19:01:08 -0700

    Removed the .cpp regression test, and added the .hpp regression test. This test is pretty simple, and just makes sure that a particular using declaration is discovered in a header file. The relevant matcher and warning were added to the actual check.

--- clang-tidy/abseil/AliasFreeHeadersCheck.cpp
+++ clang-tidy/abseil/AliasFreeHeadersCheck.cpp
@@ -24,7 +24,7 @@ void AliasFreeHeadersCheck::registerMatchers(MatchFinder *Finder) {
 
 void AliasFreeHeadersCheck::check(const MatchFinder::MatchResult &Result) {
   const auto *MatchedDecl = Result.Nodes.getNodeAs<UsingDecl>("x");
-  diag(MatchedDecl->getLocation(), "convenience aliases in header files are dangerous");
+  diag(MatchedDecl->getLocation(), "warning: convenience aliases in header files are dangerous");
 }
 
 } // namespace abseil
