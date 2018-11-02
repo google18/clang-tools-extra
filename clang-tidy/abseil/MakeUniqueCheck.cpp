edb3b62849e116434c73a89d40ec0b23d4723955
Andy Zhang <azhang@cs.hmc.edu>
2018-11-01 20:06:47 -0700

    Add diagnostics

--- clang-tidy/abseil/MakeUniqueCheck.cpp
+++ clang-tidy/abseil/MakeUniqueCheck.cpp
@@ -7,6 +7,8 @@
 //
 //===----------------------------------------------------------------------===//
 
+#include <iostream>
+#include <string>
 #include "MakeUniqueCheck.h"
 #include "clang/AST/ASTContext.h"
 #include "clang/ASTMatchers/ASTMatchFinder.h"
@@ -19,27 +21,35 @@ namespace abseil {
 
 void MakeUniqueCheck::registerMatchers(MatchFinder *Finder) {
   Finder->addMatcher(cxxConstructExpr(
-        hasType( cxxRecordDecl(hasName("std::unique_ptr"))),
+        hasType(cxxRecordDecl(hasName("std::unique_ptr"))),
         argumentCountIs(1),
-        hasArgument(0,cxxNewExpr())).bind("x"),this);
-  
+        hasArgument(0, cxxNewExpr().bind("new"))).bind("construct"),this);
 
   Finder->addMatcher(cxxMemberCallExpr(
         callee(cxxMethodDecl(
              hasName("reset"),
              ofClass(cxxRecordDecl(hasName("std::unique_ptr"))))),
         argumentCountIs(1),
-        hasArgument(0, cxxNewExpr())).bind("y"),this);
+        hasArgument(0, cxxNewExpr())).bind("reset_call"),this);
   
 }
 
 void MakeUniqueCheck::check(const MatchFinder::MatchResult &Result) {
-  const auto *MatchedConstructor = Result.Nodes.getNodeAs<CXXConstructExpr>("x");
-  if (MatchedConstructor != nullptr) {
-    diag(MatchedConstructor->getLocation(), "replace new with absl::make_unique");
+  const auto *MatchedConstructor = Result.Nodes.getNodeAs<CXXConstructExpr>("construct");
+  const auto *NewExpr = Result.Nodes.getNodeAs<CXXNewExpr>("new");
+
+  if (MatchedConstructor) {
+    SourceRange argRange = NewExpr->getDirectInitRange();
+    const SourceManager *SM = Result.SourceManager;
+    llvm::StringRef ref = Lexer::getSourceText(CharSourceRange::getCharRange(argRange), *SM, LangOptions());
+    std::string NewText = "auto VAR_NAME = absl::make_unique<" + NewExpr->getAllocatedType().getAsString() + ">" + ref.str() + ")";
+    
+    diag(MatchedConstructor->getBeginLoc(), "prefer absl::make_unique to constructing unique_ptr with new") 
+        << FixItHint::CreateReplacement(
+             CharSourceRange::getTokenRange(MatchedConstructor->getBeginLoc(), argRange.getEnd()), NewText);
   }
 
-  const auto *MatchedReset = Result.Nodes.getNodeAs<CXXMemberCallExpr>("y");
+  const auto *MatchedReset = Result.Nodes.getNodeAs<CXXMemberCallExpr>("reset_call");
   if (MatchedReset != nullptr) {  
     diag(MatchedReset->getExprLoc(), "replace reset with absl::make_unique");
   }
