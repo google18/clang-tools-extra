0c3863d21c5f611456568a872d15063756e82004
Naysh <sakmal@hmc.edu>
2018-11-14 15:50:50 -0800

    Added simple positive test for the QualifiedAliasesCheck.

--- test/clang-tidy/abseil-qualified-aliases.cpp
+++ test/clang-tidy/abseil-qualified-aliases.cpp
@@ -1,14 +1,18 @@
 // RUN: %check_clang_tidy %s abseil-qualified-aliases %t
+  
+namespace foo {
+  void f();
+  void correct();
+}
 
-// FIXME: Add something that triggers the check here.
-void f();
-// CHECK-MESSAGES: :[[@LINE-1]]:6: warning: function 'f' is insufficiently awesome [abseil-qualified-aliases]
+namespace bar {
+  using foo::f;
+// CHECK-MESSAGES: :[[@LINE-1]]:9: warning: using declaration is not fully qualified [abseil-qualified-aliases]
+  using ::foo::correct;
+}
 
-// FIXME: Verify the applied fix.
 //   * Make the CHECK patterns specific enough and try to make verified lines
 //     unique to avoid incorrect matches.
 //   * Use {{}} for regular expressions.
-// CHECK-FIXES: {{^}}void awesome_f();{{$}}
+// CHECK-FIXES: {{^}}  using ::foo::f;{{$}}
 
-// FIXME: Add something that doesn't trigger the check here.
-void awesome_f2();
