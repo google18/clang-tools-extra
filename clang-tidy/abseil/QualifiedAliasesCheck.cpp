71e194dd36b29ea21fe26b7631b9a8491daf109d
Naysh <sakmal@hmc.edu>
2018-12-06 20:53:30 -0800

    Added the implementation of the check function for Qualified Aliases. Right now, this only produces a warning. It seems like FixIts are hard in general, but could potentially be added for some specific cases with more work.

--- clang-tidy/abseil/QualifiedAliasesCheck.cpp
+++ clang-tidy/abseil/QualifiedAliasesCheck.cpp
@@ -23,8 +23,25 @@ void QualifiedAliasesCheck::registerMatchers(MatchFinder *Finder) {
 }
 
 void QualifiedAliasesCheck::check(const MatchFinder::MatchResult &Result) {
-  // FIXME: Add callback implementation.
   const auto *MatchedDecl = Result.Nodes.getNodeAs<UsingDecl>("x");
+
+  // Finds the nested-name-specifier location.
+  const NestedNameSpecifierLoc QualifiedLoc = MatchedDecl->getQualifierLoc();
+  const SourceLocation FrontLoc = QualifiedLoc.getBeginLoc();
+  
+  // Checks if the using declaration is fully qualified.
+  const SourceManager *SM = Result.SourceManager;
+  CharSourceRange FrontRange = CharSourceRange();
+  FrontRange.setBegin(FrontLoc);
+  FrontRange.setEnd(FrontLoc.getLocWithOffset(2));
+  llvm::StringRef Beg = Lexer::getSourceText(FrontRange, *SM, LangOptions());
+  
+  // If the using declaration is fully qualified, don't produce a warning. 
+  if (Beg.startswith("::"))
+    return;
+ 
+  diag(FrontLoc, "using declaration is not fully qualified: see "
+  "https://abseil.io/tips/119");
 }
 
 } // namespace abseil
