872fe714a51dd372f540a3605ae637e9276d9e81
Ywicheng <clinic18@clinic18s-Mac-mini.local>
2018-12-06 20:35:05 -0800

    Deleted unnecessary whitespaces for abseil-anonymous-enclosed-aliases check

--- clang-tidy/abseil/AnonymousEnclosedAliasesCheck.cpp
+++ clang-tidy/abseil/AnonymousEnclosedAliasesCheck.cpp
@@ -22,39 +22,39 @@ void AnonymousEnclosedAliasesCheck::registerMatchers(MatchFinder *Finder) {
   // 1. anonymous namespace declarations,
   // 2. using declarations that are not inside an anonymous declaration
   Finder->addMatcher(
-  namespaceDecl(isAnonymous()).bind("anonymous_namespace"), this);
+    namespaceDecl(isAnonymous()).bind("anonymous_namespace"), this);
   Finder->addMatcher(
-  usingDecl(unless(hasAncestor(
-  namespaceDecl(isAnonymous())))).bind("using_decl"), this);
+    usingDecl(unless(hasAncestor(
+      namespaceDecl(isAnonymous())))).bind("using_decl"), this);
 }
 
 void AnonymousEnclosedAliasesCheck::check(const MatchFinder::MatchResult &Result) {
 
   const auto *MatchedUsingDecl =
-  Result.Nodes.getNodeAs<UsingDecl>("using_decl");
+    Result.Nodes.getNodeAs<UsingDecl>("using_decl");
   // If a potential using declaration is matched,
   if (MatchedUsingDecl) {
-  // and if an anonymous namespace declaration has already been found,
-  // the matched using declaration is a target, and we print out 
-  // the diagnostics for it. Otherwise, we add the using declaration
-  // to the vector containing all candidate using declarations.
-  	if (AnonymousNamespaceDecl) {
-  		diag(MatchedUsingDecl->getLocation(), 
-  			"UsingDecl %0 should be in the anonymous namespace") 
+    // and if an anonymous namespace declaration has already been found,
+    // the matched using declaration is a target, and we print out
+    // the diagnostics for it. Otherwise, we add the using declaration
+    // to the vector containing all candidate using declarations.
+    if (AnonymousNamespaceDecl) {
+  		diag(MatchedUsingDecl->getLocation(),
+  			"UsingDecl %0 should be in the anonymous namespace")
   			<< MatchedUsingDecl;
 		} else {
 			MatchedUsingDecls.push_back(MatchedUsingDecl);
 		}
 		return;
   }
-  // Otherwise, an anonymous namespace declaration is matched. In this case, 
-  // all the previously matched namespace declarations in the vector 
-  // CurrentUsingDecl are our targets, and we print out the 
+  // Otherwise, an anonymous namespace declaration is matched. In this case,
+  // all the previously matched namespace declarations in the vector
+  // CurrentUsingDecl are our targets, and we print out the
   // diagnostics for all of them.
-  AnonymousNamespaceDecl = 
+  AnonymousNamespaceDecl =
   	Result.Nodes.getNodeAs<NamespaceDecl>("anonymous_namespace");
   for (const auto* CurrentUsingDecl: MatchedUsingDecls) {
-  	diag(CurrentUsingDecl->getLocation(), 
+  	diag(CurrentUsingDecl->getLocation(),
   		"UsingDecl %0 should be in the anonymous namespace. See: "
       "https://abseil.io/tips/119")
   		<< CurrentUsingDecl;
