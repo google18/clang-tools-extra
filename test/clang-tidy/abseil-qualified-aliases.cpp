53c44bd08fa61f02db77225faa6fb769b983b932
Naysh <sakmal@hmc.edu>
2018-12-06 20:56:41 -0800

    Added test file with two positive cases and one negative case for the Qualified Aliases check.

--- test/clang-tidy/abseil-qualified-aliases.cpp
+++ test/clang-tidy/abseil-qualified-aliases.cpp
@@ -1,14 +1,26 @@
 // RUN: %check_clang_tidy %s abseil-qualified-aliases %t
+  
+namespace foo {
+  void f();
+  void correct();
+}
 
-// FIXME: Add something that triggers the check here.
-void f();
-// CHECK-MESSAGES: :[[@LINE-1]]:6: warning: function 'f' is insufficiently awesome [abseil-qualified-aliases]
+namespace bar {
+  using foo::f;
+// CHECK-MESSAGES: :[[@LINE-1]]:9: warning: using declaration is not fully qualified: see https://abseil.io/tips/119 [abseil-qualified-aliases]
+  using ::foo::correct;
+}
 
-// FIXME: Verify the applied fix.
-//   * Make the CHECK patterns specific enough and try to make verified lines
-//     unique to avoid incorrect matches.
-//   * Use {{}} for regular expressions.
-// CHECK-FIXES: {{^}}void awesome_f();{{$}}
+namespace outermost {
+  namespace middle {
+    namespace innermost {
 
-// FIXME: Add something that doesn't trigger the check here.
-void awesome_f2();
+      enum Color {Red, Blue, Yellow};
+
+    } // namespace innermost
+
+    using innermost::Color;
+// CHECK-MESSAGES: :[[@LINE-1]]:11: warning: using declaration is not fully qualified: see https://abseil.io/tips/119 [abseil-qualified-aliases]
+
+  } // namespace middle
+} // namespace example
