5865b0061359e5d01bbac86a6fe2c94d1353036e
Andy Zhang <azhang@cs.hmc.edu>
2019-04-18 11:24:25 -0700

    Trim tests

--- test/clang-tidy/abseil-make-unique.cpp
+++ test/clang-tidy/abseil-make-unique.cpp
@@ -27,7 +27,6 @@ struct Derived : public Base {
 };
 
 int* returnPointer();
-void expectPointer(std::unique_ptr<int>);
 
 std::unique_ptr<int> makeAndReturnPointer() {
   return std::unique_ptr<int>(new int(0));
@@ -52,50 +51,6 @@ void Positives() {
   P2.reset(new A(3, 4));
   // CHECK-MESSAGES: :[[@LINE-1]]:6: warning: use absl::make_unique instead [abseil-make-unique]
   // CHECK-FIXES: P2 = absl::make_unique<A>(3, 4);
-
-  // No arguments to new expression
-  std::unique_ptr<int> P3 = std::unique_ptr<int>(new int);
-  // CHECK-MESSAGES: :[[@LINE-1]]:29: warning: use absl::make_unique instead [abseil-make-unique]
-  // CHECK-FIXES: std::unique_ptr<int> P3 = absl::make_unique<int>();
-
-  P3.reset(new int);
-  // CHECK-MESSAGES: :[[@LINE-1]]:6: warning: use absl::make_unique instead [abseil-make-unique]
-  // CHECK-FIXES: P3 = absl::make_unique<int>();
-
-  // Nested parentheses
-  std::unique_ptr<int> P4 = std::unique_ptr<int>((new int(3)));
-  // CHECK-MESSAGES: :[[@LINE-1]]:29: warning: use absl::make_unique instead [abseil-make-unique]
-  // CHECK-FIXES: std::unique_ptr<int> P4 = absl::make_unique<int>(3);
-
-  P4 = std::unique_ptr<int>(((new int(4))));
-  // CHECK-MESSAGES: :[[@LINE-1]]:8: warning: use absl::make_unique instead [abseil-make-unique]
-  // CHECK-FIXES: P4 = absl::make_unique<int>(4);
-
-  P4.reset((new int(5)));
-  // CHECK-MESSAGES: :[[@LINE-1]]:6: warning: use absl::make_unique instead [abseil-make-unique]
-  // CHECK-FIXES: P4 = absl::make_unique<int>(5);
-
-  // With auto
-  auto P5 = std::unique_ptr<int>(new int());
-  // CHECK-MESSAGES: :[[@LINE-1]]:13: warning: use absl::make_unique instead [abseil-make-unique]
-  // CHECK-FIXES: auto P5 = absl::make_unique<int>();
-
-  {
-    // No std
-    using namespace std;
-    unique_ptr<int> Q = unique_ptr<int>(new int());
-    // CHECK-MESSAGES: :[[@LINE-1]]:25: warning: use absl::make_unique instead [abseil-make-unique]
-    // CHECK-FIXES: unique_ptr<int> Q = absl::make_unique<int>();
-
-    Q = unique_ptr<int>(new int());
-    // CHECK-MESSAGES: :[[@LINE-1]]:9: warning: use absl::make_unique instead [abseil-make-unique]
-    // CHECK-FIXES: Q = absl::make_unique<int>();
-  }
-
-  // Create the unique_ptr as a parameter to a function
-  expectPointer(std::unique_ptr<int>(new int()));
-  // CHECK-MESSAGES: :[[@LINE-1]]:17: warning: use absl::make_unique instead [abseil-make-unique]
-  // CHECK-FIXES: expectPointer(absl::make_unique<int>());
 }
 
 void Negatives() {
