f1905d472388d8cd45b553a9f546fdab7abb26fd
Ywicheng <clinic18@clinic18s-Mac-mini-2.local>
2019-04-17 17:49:59 -0700

    Finished implementing abseil-anonymous-enclosed-aliases check

--- clang-tidy/abseil/AnonymousEnclosedAliasesCheck.cpp
+++ clang-tidy/abseil/AnonymousEnclosedAliasesCheck.cpp
@@ -18,20 +18,30 @@ namespace tidy {
 namespace abseil {
 
 void AnonymousEnclosedAliasesCheck::registerMatchers(MatchFinder *Finder) {
-  // We try to match two nodes:
+  // We try to match two types of nodes:
   // 1. anonymous namespace declarations,
-  // 2. using declarations that are not inside an anonymous declaration
+  // 2. using declarations that are not inside an anonymous namespace and
+  // not inside any other scope.
   Finder->addMatcher(
-    namespaceDecl(isAnonymous()).bind("anonymous_namespace"), this);
+      namespaceDecl(allOf(namespaceDecl(isAnonymous()),
+                          namespaceDecl(isExpansionInMainFile())))
+          .bind("anonymous_namespace"),
+      this);
   Finder->addMatcher(
-    usingDecl(unless(hasAncestor(
-      namespaceDecl(isAnonymous())))).bind("using_decl"), this);
+      usingDecl(
+          allOf(usingDecl(isExpansionInMainFile()),
+                usingDecl(unless(hasAncestor(functionDecl()))),
+                usingDecl(unless(hasAncestor(cxxRecordDecl()))),
+                usingDecl(unless(hasAncestor(namespaceDecl(isAnonymous()))))))
+          .bind("using_decl"),
+      this);
 }
 
-void AnonymousEnclosedAliasesCheck::check(const MatchFinder::MatchResult &Result) {
+void AnonymousEnclosedAliasesCheck::check(
+    const MatchFinder::MatchResult &Result) {
 
   const auto *MatchedUsingDecl =
-    Result.Nodes.getNodeAs<UsingDecl>("using_decl");
+      Result.Nodes.getNodeAs<UsingDecl>("using_decl");
   // If a potential using declaration is matched,
   if (MatchedUsingDecl) {
     // and if an anonymous namespace declaration has already been found,
@@ -39,27 +49,29 @@ void AnonymousEnclosedAliasesCheck::check(const MatchFinder::MatchResult &Result
     // the diagnostics for it. Otherwise, we add the using declaration
     // to the vector containing all candidate using declarations.
     if (AnonymousNamespaceDecl) {
-  		diag(MatchedUsingDecl->getLocation(),
-  			"UsingDecl %0 should be in the anonymous namespace. See: "
-        "https://abseil.io/tips/119")
-  			<< MatchedUsingDecl;
-		} else {
-			MatchedUsingDecls.push_back(MatchedUsingDecl);
-		}
-		return;
+      diag(MatchedUsingDecl->getLocation(),
+           "using declaration %0 should be in the anonymous namespace. "
+           "Use discretion when moving using declarations as it might "
+           "necessitate moving lines containing relevant aliases.")
+          << MatchedUsingDecl;
+    } else {
+      MatchedUsingDecls.push_back(MatchedUsingDecl);
+    }
+    return;
   }
   // Otherwise, an anonymous namespace declaration is matched. In this case,
   // all the previously matched namespace declarations in the vector
   // CurrentUsingDecl are our targets, and we print out the
   // diagnostics for all of them.
   AnonymousNamespaceDecl =
-  	Result.Nodes.getNodeAs<NamespaceDecl>("anonymous_namespace");
-  for (const auto* CurrentUsingDecl: MatchedUsingDecls) {
-  	diag(CurrentUsingDecl->getLocation(),
-  		"UsingDecl %0 should be in the anonymous namespace. See: "
-      "https://abseil.io/tips/119")
-  		<< CurrentUsingDecl;
-	}
+      Result.Nodes.getNodeAs<NamespaceDecl>("anonymous_namespace");
+  for (const auto *CurrentUsingDecl : MatchedUsingDecls) {
+    diag(CurrentUsingDecl->getLocation(),
+         "using declaration %0 should be in the anonymous namespace. "
+         "Use discretion when moving using declarations as it might "
+         "necessitate moving lines containing relevant aliases.")
+        << CurrentUsingDecl;
+  }
 }
 
 } // namespace abseil
--- test/clang-tidy/abseil-anonymous-enclosed-aliases.cpp
+++ test/clang-tidy/abseil-anonymous-enclosed-aliases.cpp
@@ -1,27 +1,56 @@
 // RUN: %check_clang_tidy %s abseil-anonymous-enclosed-aliases %t
 namespace bar {
 
-class something {
+class A {};
+class B {};
 
-};
-}
+} // namespace bar
 
-namespace foo {
+namespace foo1 {
 
-using bar::something;
+// CHECK-MESSAGES: :[[@LINE+4]]:12: warning: using declaration 'A' should
+// be in the anonymous namespace. Use discretion when moving using declarations
+// as it might necessitate moving lines containing relevant aliases.
+// [abseil-anonymous-enclosed-aliases]
+using bar::A;
+void f(A a);
+
+namespace {} // anonymous namespace
+
+} // namespace foo1
+
+namespace foo2 {
 
 namespace {
 
-}  // anonymous namespace
-}  // namespace foo
-// CHECK-MESSAGES: :[[@LINE-6]]:12: warning: UsingDecl 'something' should be in the anonymous namespace. See: https://abseil.io/tips/119 [abseil-anonymous-enclosed-aliases]
+// This is okay
+using ::bar::B;
 
+} // anonymous namespace
 
-namespace foo {
+void g(B b);
 
-namespace {
+} // namespace foo2
 
-using ::bar::something;
+// Check should not be triggered below when the using declaration is at
+// function or class (instead of namespace) scope.
+namespace outer {
 
-}  // anonymous namespace
-}  // namespace foo
+int fun_scope() {
+  using ::bar::A;
+  return 0;
+} // function scope
+
+class Base {
+public:
+  void f();
+}; // class scope
+
+class Derived : public Base {
+public:
+  using Base::f;
+}; // class scope
+
+namespace {} // anonymous namespace
+
+} // namespace outer
